09ab5fbf5a7bfa0999d29766391e67a5972acd49
Jiasheng Zhou <zhoujiasheng2012@gmail.com>
2018-03-21 17:10:51 +0800

    render courses by categories

--- client/components/admin/courses/admin_course_list.jsx
+++ client/components/admin/courses/admin_course_list.jsx
@@ -3,7 +3,6 @@ import {GridList, GridTile} from 'material-ui/GridList'
 import IconButton from 'material-ui/IconButton';
 import AdminCourseListCell from './admin_course_list_cell';
 
-
 const styles = {
     root: {
       display: 'flex',
@@ -15,19 +14,55 @@ const styles = {
       flexWrap: 'nowrap',
     }
   };
-const AdminCourseList = ({courses}) => {
+
+const getCategoryNameFromId = (categories, id) => {
+    for(let i = 0; i<categories.length; i++) {
+        if(categories[i]._id === id) {
+            return categories[i].name;
+        }
+    }
+}
+
+const addCategoryToCourses = (courses, categories) => {
+    let categoryCourses = {};
+    courses.forEach(course => {
+        const categoryName = getCategoryNameFromId(categories, course.categoryId);
+        if(categoryCourses[categoryName]) {
+            categoryCourses[categoryName].push(course);
+        } else {
+            categoryCourses[categoryName] = [course];
+        }
+    });
+    return categoryCourses;
+}
+
+const AdminCourseList = ({courses, categories}) => {
+    const categoryAndCourses = addCategoryToCourses(courses, categories);
         return <div>
         <h3>所有课程</h3>
-        <div>
-            <GridList style={styles.gridList}>
-                {courses.map((course) => (
-                    <AdminCourseListCell
-                        key={course._id}
-                        course={course}>
-                        onDeleteCourse={this.onDeleteCourse}
-                    </AdminCourseListCell>
-                ))}
-            </GridList>
+        <div style={styles.root}>
+            {
+                categories.map( category => {
+                    const courses = categoryAndCourses[category.name];
+                    if(courses) {
+                        return <div key={category.name}>
+                            <h2>{category.name}</h2>
+                                <GridList style={styles.gridList}>
+                                    {courses.map((course) => (
+                                        <AdminCourseListCell
+                                            key={course._id}
+                                            course={course}>
+                                            onDeleteCourse={this.onDeleteCourse}
+                                        </AdminCourseListCell>
+                                    ))}
+                                </GridList>
+                        </div>
+                    } else {
+                        return <div></div>
+                    }
+                    
+                })
+            }
         </div>
     </div>
 }
--- client/components/admin/courses/admin_course_list_container.jsx
+++ client/components/admin/courses/admin_course_list_container.jsx
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor'; 
 import { withTracker } from 'meteor/react-meteor-data';
 import AdminCourseList from './admin_course_list';
-import {getCourses} from '../../../../import/service/course_service';
+import {getCourses,getCategories} from '../../../../import/service/course_service';
 
 const AdminCourseListContainer = withTracker(({schoolId}) => {
     const courses = Meteor.subscribe('courses', schoolId).ready() ? getCourses(schoolId) : [];
+    const categories = Meteor.subscribe('categories', schoolId).ready() ? getCategories(schoolId) : [];
     return {
-        courses
+        courses,
+        categories
     }
 })(AdminCourseList);
 
